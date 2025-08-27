188733392e3ab8e36a1f4d0f762723f8c59e9ea9
alexiskayondekonda <alexiskayondekonda@gmail.com>
2025-08-27 11:42:09 +0100

    Remove unwanted subfolder

--- hopital_project/gestion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Patient, Medecin, Service
-
-admin.site.register(Patient)
-admin.site.register(Medecin)
-admin.site.register(Service)
--- hopital_project/gestion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.liste_patients, name='liste_patients'),
-    path('ajouter/', views.ajouter_patient, name='ajouter_patient'),
-    path('modifier/<int:patient_id>/', views.modifier_patient, name='modifier_patient'),
-    path('supprimer/<int:patient_id>/', views.supprimer_patient, name='supprimer_patient'),
-]
